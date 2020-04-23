4348b15eb679187b124a1b1bb1449d78ec893ef7
lodewijkvkeizerswaard <lodewijk.keizer@gmail.com>
2020-04-23 15:34:31 +0200

    Added logging to the Latent Scorer

--- src/main/java/io/anserini/search/latent/LatentScorer.java
+++ src/main/java/io/anserini/search/latent/LatentScorer.java
@@ -12,6 +12,9 @@ import org.apache.lucene.search.LeafSimScorer;
 import org.apache.lucene.search.ImpactsDISI;
 import org.apache.lucene.search.Weight;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class LatentScorer extends Scorer{
     private final PostingsEnum postingsEnum;
     private final ImpactsEnum impactsEnum;
@@ -19,12 +22,16 @@ public class LatentScorer extends Scorer{
     private final LeafSimScorer docScorer;
     private final ImpactsDISI impactsDisi;
 
+    private static final Logger LOG = LogManager.getLogger(LatentScorer.class);
+
     public LatentScorer(Weight weight, PostingsEnum postingsEnum, LeafSimScorer docScorer) {
         super(weight);
         iterator = this.postingsEnum = postingsEnum;
         impactsEnum = new SlowImpactsEnum(postingsEnum);
         impactsDisi = new ImpactsDISI(impactsEnum, impactsEnum, docScorer.getSimScorer());
         this.docScorer = docScorer;
+
+        LOG.info("[LatentScorer] Init! Weight: " + weight.toString() + "\npostingsEnum: " + postingsEnum.toString() + "\nLeafSimScorer: " + docScorer.toString());
     }
 
     @Override
@@ -40,6 +47,7 @@ public class LatentScorer extends Scorer{
     @Override
     public float score() throws IOException {
         assert docID() != DocIdSetIterator.NO_MORE_DOCS;
+        LOG.info("[LatentScorer] Score: " + postingsEnum.toString());
         return docScorer.score(postingsEnum.docID(), postingsEnum.freq());
     }
     @Override
