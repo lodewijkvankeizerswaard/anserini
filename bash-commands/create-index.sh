658084779d07349c81647376e48c33eb1b39898a
lodewijkvkeizerswaard <lodewijk.keizer@gmail.com>
2020-06-04 17:03:13 +0200

    This works: append dummy slr to normal index

--- bash-commands/create-index.sh
+++ bash-commands/create-index.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-sh ./target/appassembler/bin/IndexCollection -collection JsonCollection -input data/collection -generator SLRGenerator -storeSLR -threads 8 -index data/collection-index
+sh ./target/appassembler/bin/IndexCollection -collection JsonCollection -input data/msmarco-pas -slr -slr.append -threads 8 -index data/msmarco-pas-index
