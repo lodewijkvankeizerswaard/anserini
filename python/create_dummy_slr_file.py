c88427782818566c3f3aa47b9dff419f1e24fd6a
lodewijkvkeizerswaard <lodewijk.keizer@gmail.com>
2020-06-08 17:35:45 +0200

    Now writes for doc id's from a trec collection

--- python/create_dummy_slr_file.py
+++ python/create_dummy_slr_file.py
@@ -1,29 +1,54 @@
 import numpy as np
 import subprocess
 
-def np_arr_to_tsv(arr):
+def dummy_slr_arr():
+    slr = np.random.uniform(0, 1, size=1000)
+    slr[slr <= 0.9] = 0
+    return slr
+
+def slr_to_string(arr):
     s = ""
     for val in arr:
         s += str(val) + "\t"
     return s
 
+def write_slr_line(file, docid, slr):
+    f.write(str(docid) + "\t" + slr_to_string(slr) + "\n")
+
+
 if __name__ == "__main__":
-    filename = "data/dummy_slr_file.txt"
+    doc_id_list = []
+    doc_id_file = open("data/robust04/FBIS/FB396001", "r")
+
+    for line in doc_id_file:
+        # print(line)
+        try:
+            doc_id_tag = line.index("DOCNO")
+            if(doc_id_tag):
+                doc_id_list.append(line.split(" ")[1])
+                # print()
+        except:
+            pass
+    
+    doc_id_file.close()
+
+    filename = "data/dummy_slr_robust04.txt"
     f = open(filename, "a")
 
-    line = subprocess.check_output(['tail', '-1', filename]).decode('ascii')
-    start = int(line.split('\t')[0])
-    nr_docs = 8000000
+    
 
-    print("Start:" + str(start))
-    print("End:" + str(nr_docs))
+    # last_doc_line = subprocess.check_output(['tail', '-1', filename]).decode('ascii')
+    # start_docid = int(last_doc_line.split('\t')[0])
+    # nr_docs = 8000000
 
-    for i in range(start+1, nr_docs):
-        slr = np.random.uniform(0, 1, size=1000)
-        slr[slr <= 0.9] = 0
-        f.write(str(i) + "\t" + np_arr_to_tsv(slr) + "\n")
+    # print("Start:" + str(start))
+    # print("End:" + str(nr_docs))
 
-        if i % 100000 == 0:
-            print("Pogres: " + str(i/(nr_docs - start + 1)*100) + "%")
+    for doc_id in doc_id_list:
+        
+        write_slr_line(f, doc_id, dummy_slr_arr())
+
+        # if i % 100000 == 0:
+        #     print("Pogres: " + str(i/(nr_docs - start + 1)*100) + "%")
     
     f.close()
