983bc91e9dc1e167cb2e3e4359030905bdbc0f4c
lodewijkvkeizerswaard <lodewijk.keizer@gmail.com>
2020-06-10 12:14:14 +0200

    slr file to json server version 2

--- python/slr_tsv_to_json.py
+++ python/slr_tsv_to_json.py
@@ -13,10 +13,9 @@ fr = open(input_folder + input_file, 'r')
 print("Reading from file: " + input_folder + input_file)
 
 line_count = 0
-file_count = 0
+file_nr = 0
 
 output_file_base = input_file[: input_file.rfind('.')] + "_"
-output_file_path = output_file_base 
 fw = open(output_file_path + "0.json", 'a')
 
 print("Writing to file: " + output_file_path + "0.json")
@@ -26,22 +25,19 @@ doc_list = []
 for line in fr:
     line_id = line.split("\t")[0]
     line_vals = line[ line.index("\t") + 1 :]
-    val_dict = {'id' : line_id, 'contents' : line_vals[0:10]}
+    val_dict = {'id' : line_id, 'contents' : line_vals}
     doc_list.append(val_dict)
 
     line_count += 1
     if line_count >= docs_per_file:
         fw.write(json.dumps(doc_list))
         fw.close()
+        file_nr += 1
         file_suffix = str(file_count * docs_per_file) + ".json"
-        print("Writing to file: " + output_file_path + file_suffix)
-        fw = open(output_file_path + file_suffix, 'a')
+        print("Writing to file: " + output_file_base + file_suffix)
+        fw = open(output_file_base + file_suffix, 'a')
 
-        file_count += 1
         line_count = 0
-    
-    if file_count > 3:
-        break
 
 fr.close()
 fw.close()
