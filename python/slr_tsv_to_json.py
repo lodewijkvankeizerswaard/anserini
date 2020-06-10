35c606d9a011f981aa9ea8b2e934cb647a8e21c9
lodewijkvkeizerswaard <lodewijk.keizer@gmail.com>
2020-06-10 11:33:48 +0200

    slr file to json version 3

--- python/slr_tsv_to_json.py
+++ python/slr_tsv_to_json.py
@@ -1,3 +1,4 @@
+import json
 
 fr = open("data/dummy_slr_file.txt", 'r')
 fw = open("data/dummy_slr_file_0.tsv", 'a')
@@ -7,9 +8,10 @@ line_count = 0
 for line in fr:
     line_id = line.split("\t")[0]
     line_vals = line[ line.index("\t") :]
-    val_dict = {'id' : line_id, 'contents' : line_vals}
+    val_dict = {'id' : line_id, 'contents' : line_vals[0:10]}
 
-    print(val_dict)
+
+    print(json.dumps(val_dict))
 
 fr.close()
 fw.close()
