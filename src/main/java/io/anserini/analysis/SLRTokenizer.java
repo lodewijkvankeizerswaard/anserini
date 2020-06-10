f19bf765f200dd26b5dc57667aaadcea62027fab
lodewijkvkeizerswaard <lodewijk.keizer@gmail.com>
2020-06-10 15:28:36 +0200

    debug float format in tokenizer

--- src/main/java/io/anserini/analysis/SLRTokenizer.java
+++ src/main/java/io/anserini/analysis/SLRTokenizer.java
@@ -202,13 +202,16 @@ public class SLRTokenizer extends Tokenizer{
                 valueBuffer[i] = (Character.isDigit(buffer[i + decimalStart]) ) ? buffer[i + decimalStart] : '0';
             }
         } else {
-            LOG.info(String.valueOf(tokenBuffer));
             char[] exponentChar = {buffer[exponentStart + 1], buffer[exponentStart + 2]};
             String exponentStr = String.valueOf(exponentChar);
+            LOG.info("input:" + String.valueOf(buffer) + " expo:" + exponentStr);
             Integer exponent = Integer.parseInt(exponentStr);
+            
+
             for(int i = 0; i < tokenBuffer.length; i++) {
                 tokenBuffer[i] = (i < exponent - 1) ? '0' : buffer[i - exponent - 1];
             }
+            LOG.info(String.valueOf(tokenBuffer));
         }
     }
     
