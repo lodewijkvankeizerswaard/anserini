8eec4472866dc0b41b2cb358825cc4b0d9d085a0
lodewijkvkeizerswaard <lodewijk.keizer@gmail.com>
2020-06-10 13:33:41 +0200

    debugging empty slrMap

--- src/main/java/io/anserini/index/generator/SLRGenerator.java
+++ src/main/java/io/anserini/index/generator/SLRGenerator.java
@@ -105,8 +105,8 @@ public class SLRGenerator<T extends SourceDocument> implements LuceneDocumentGen
     slrMap.clear();
     String[] splitValues = content.split("\\t");
     for(int i = 0; i < splitValues.length; i++) {
-      LOG.info(splitValues[i]);
-      if(Float.parseFloat(splitValues[i]) != 0)
+      // LOG.info(splitValues[i]);
+      if(splitValues[i] != null && !splitValues[i].isEmpty() && Float.parseFloat(splitValues[i]) != 0)
         slrMap.put(Integer.toString(i), splitValues[i]);
     }
 }
