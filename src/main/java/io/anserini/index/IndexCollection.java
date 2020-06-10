783f55688f36d033bedb3ec752eda73e68d53a80
lodewijkvkeizerswaard <lodewijk.keizer@gmail.com>
2020-06-10 15:56:48 +0200

    Change max document lenght

--- src/main/java/io/anserini/index/IndexCollection.java
+++ src/main/java/io/anserini/index/IndexCollection.java
@@ -791,7 +791,7 @@ public final class IndexCollection {
       config.setUseCompoundFile(false);
       config.setMergeScheduler(new ConcurrentMergeScheduler());
 
-      writer = new IndexWriter(dir, config);
+      writer = new IndexWriter(dir, config, IndexWriter.MaxFieldLength(Integer.MAX_VALUE));
     }
 
     final ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(numThreads);
