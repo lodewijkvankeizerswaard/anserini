7c2470caeadd788846cb92813cf90840169c58ca
lodewijkvkeizerswaard <lodewijk.keizer@gmail.com>
2020-06-11 16:08:50 +0200

    SLRGenerator formats floats correctly

--- src/main/java/io/anserini/index/generator/SLRGenerator.java
+++ src/main/java/io/anserini/index/generator/SLRGenerator.java
@@ -125,11 +125,8 @@ public class SLRGenerator<T extends SourceDocument> implements LuceneDocumentGen
         try {
           if(Float.parseFloat(splitValues[i]) != 0) {
             String mapValue = normalizeFloatFormat(splitValues[i]);
-            LOG.info("input  value:  " + splitValues[i]);
-            LOG.info("adding value: (" + i + ", " + mapValue + ")");
             slrMap.put(Integer.toString(i), mapValue);
           }
-            
         } catch(Exception e) { }
       }  
     }
